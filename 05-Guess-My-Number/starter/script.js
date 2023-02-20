c2351e0cab86a5ec31d722df3e99ccadda45dd99
Jake Walters <jakepwalters@outlook.com>
2023-02-20 16:49:10 -0400

    pc changes

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -11,6 +11,10 @@
 // console.log(document.querySelector('.guess').value);
 
 const number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.number').textContent = number;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -18,5 +22,30 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = 'Enter a Number!';
+  } else if (guess === number) {
+    document.querySelector('.message').textContent = 'Correct! Good Guess';
+    highscore = score;
+    document.querySelector('.highscore').textContent = highscore;
+  } else if (guess > number) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < number) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess > 20 || guess < 1) {
+    document.querySelector('.message').textContent =
+      'Enter a number between 1 and 20!';
   }
 });
