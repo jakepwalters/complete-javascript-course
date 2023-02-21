545dff45996db60513f2662aa08de964cebb1167
Jake Walters <jakepwalters@outlook.com>
2023-02-21 16:58:39 -0400

    changes mac

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,42 +10,69 @@
 // document.querySelector('.guess').value = 23
 // console.log(document.querySelector('.guess').value);
 
-const number = Math.trunc(Math.random() * 20) + 1;
+let number = Math.trunc(Math.random() * 20) + 1;
+//to make it easier to test winning'
+console.log(number);
 let score = 20;
 let highscore = 0;
 
-document.querySelector('.number').textContent = number;
+const displayMessage = function(message) {
+  document.querySelector('.message').textContent = message
+}
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess);
 
+  // when player doesn't enter a number
   if (!guess) {
-    document.querySelector('.message').textContent = 'Enter a Number!';
+    displayMessage('Enter a Number!');
+
+    //when player wins
   } else if (guess === number) {
-    document.querySelector('.message').textContent = 'Correct! Good Guess';
-    highscore = score;
-    document.querySelector('.highscore').textContent = highscore;
-  } else if (guess > number) {
+    displayMessage('Correct! Good Guess');
+    
+
+    //change CSS properties
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    //show player the number after they've guess it correctly
+    document.querySelector('.number').textContent = number;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    };
+
+    //when guess is wrong
+   } else if (guess !== number) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too high!';
+      displayMessage(guess > number ? 'Too high!' : 'Too low!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'You lost!';
+      displayMessage('You lost!');
       document.querySelector('.score').textContent = 0;
     }
-  } else if (guess < number) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lost!';
-      document.querySelector('.score').textContent = 0;
-    }
-  } else if (guess > 20 || guess < 1) {
-    document.querySelector('.message').textContent =
-      'Enter a number between 1 and 20!';
+  
   }
+});
+
+//player clicks 'Again'
+
+document.querySelector('.again').addEventListener('click', function () {
+  //restore variables to initial values
+  number = Math.trunc(Math.random() * 20) + 1;
+  console.log(number);
+  score = 20;
+
+  //restore all DOM props to initial values
+  displayMessage('Start guessing...')
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = score;
+
+  //restore css props
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
+const showModal = document.querySelectorAll('.show-modal');
+
+const exitModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener('click', openModal);
+}
+
+closeModal.addEventListener('click', exitModal);
+overlay.addEventListener('click', exitModal);
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    exitModal();
+  }
+});
