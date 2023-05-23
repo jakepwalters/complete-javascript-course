bf55e9691546d885fcf180bb38d4fff4ef305b43
Jake Walters <jakepwalters@outlook.com>
2023-05-23 20:00:09 -0300

    push

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -65,12 +65,84 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
+/////////////////////////////////////////////////
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+
+//SLICE
+
+// console.log(arr.slice(2));
+// console.log(arr.slice(2, 4));
+// console.log(arr.slice(-1));
+// console.log(arr.slice(1, -2));
+// console.log(arr.slice());
+
+// //SPLICE
+
+// // console.log(arr.splice(2));
+// console.log(arr.splice(-1));
+// console.log(arr);
+
+// //REVERSE
+
+// arr = ['a', 'b', 'c', 'd', 'e'];
+// const arr2 = ['d', 'f', 'g', 'h', 'j'];
+
+// console.log(arr2.reverse());
+// console.log(arr2);
+
+// //CONCATE
+
+// const letters = arr.concat(arr2);
+// console.log(letters);
+// console.log([...arr, ...arr2]);
+
+// //JOIN
+
+// console.log(letters.join(' - '));
+
+// AT
+
+// const arr = [23, 11, 64];
+
+// console.log(arr.at(0));
+
+// //getting the last element
+// console.log(arr.at(arr.length - 1));
+// console.log(arr.slice(-1)[0]);
+// console.log(arr.at(-1));
+
+// console.log('jake'.at(0));
+
+// /// FOR EACH
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// // for (const movement of movements) {
+// //   if (movement > 0) {
+// //     console.log(`You deposited ${movement}`);
+// //   } else {
+// //     console.log(`You withdrew ${Math.abs(movement)}`);
+// //   }
+// // }
+
+// movements.forEach(function (movement, index, array) {
+//   if (movement > 0) {
+//     console.log(`Movement ${index + 1}: You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${index + 1}: You withdrew ${Math.abs(movement)}`);
+//   }
+// });
+
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
 
-/////////////////////////////////////////////////
+const currenciesUnique = new Set(['USD', 'GBP', 'EUR', 'USD', 'EUR']);
+console.log(currenciesUnique);
